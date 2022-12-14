f99e60d681c966365f158aa3fe3e81dc149fb3b1
cjkg <cjgibilisco@gmail.com>
2022-12-13 20:51:18 -0600

    added PICO-8 font, fixed bin files to work with heroku (I hope)

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Discemori
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
+
+    config.assets.paths << Rails.root.join("assets", "fonts")
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
